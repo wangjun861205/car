e9810cefb8629dac57e37125e2a36c861eb7776b
wangjun <wangjun20110523@gmail.com>
2021-03-03 21:43:43 +0800

    complete balancer

--- balancer/build/balancer.go
+++ balancer/build/balancer.go
@@ -10,19 +10,17 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-func compare(ld, rd *driver.Driver, lb, rb *balancer.Balancer, lduty, rduty uint64) (ls, rs uint64) {
+func compare(ld, rd *driver.Driver, b *balancer.Balancer, lduty, rduty uint64) (ls, rs float64) {
 	ld.Brake()
 	rd.Brake()
-	lb.Reset()
-	lb.Reset()
+	b.Reset()
 	ld.Forward(lduty)
 	rd.Forward(rduty)
 	time.Sleep(5 * time.Second)
-	ls, rs = lb.GetSpeed(), rb.GetSpeed()
+	ls, rs = b.LeftSpeed(), b.RightSpeed()
 	ld.Brake()
 	rd.Brake()
-	lb.Reset()
-	rb.Reset()
+	b.Reset()
 	return
 }
 
@@ -30,10 +28,11 @@ func main() {
 	if err := rpio.Open(); err != nil {
 		panic(err)
 	}
-	lb := balancer.NewBalancer(17, 27)
-	rb := balancer.NewBalancer(22, 26)
-	go lb.Run()
-	go rb.Run()
+	b, err := balancer.NewBalancer(17, 27, 26, 22)
+	if err != nil {
+		panic(err)
+	}
+	go b.Run()
 	ld, err := driver.NewDriver(rpio.Pin(24), rpio.Pin(23), 1, 1000)
 	if err != nil {
 		fmt.Println(err)
@@ -46,16 +45,16 @@ func main() {
 	}
 	for i := 4; i < 10; i++ {
 		left, right := uint64(i*100), uint64(i*100)
-		var leftSpeed, rightSpeed uint64
+		var leftSpeed, rightSpeed float64
 		for {
 			ld.Forward(left)
 			rd.Forward(right)
 			time.Sleep(time.Millisecond * 100)
-			lb.Reset()
-			rb.Reset()
+			b.Reset()
 			time.Sleep(time.Second)
-			ls, rs := lb.GetSpeed(), rb.GetSpeed()
-			diff := math.Abs(float64(ls-rs) / float64(ls+rs) * 1000)
+			ls, rs := b.LeftSpeed(), b.RightSpeed()
+			fmt.Printf("left speed: %.4f, right speed: %.4f\n", ls, rs)
+			diff := math.Abs(ls-rs) / math.Abs(ls+rs) * 1000
 			if diff <= 5 {
 				leftSpeed, rightSpeed = ls, rs
 				break
@@ -71,12 +70,10 @@ func main() {
 				continue
 			}
 		}
-		fmt.Printf("level: %d, left pwm: %d, right pwm: %d, left speed: %d, right speed: %d, left rate: %2f, right rate: %2f\n", i*100, left, right, leftSpeed, rightSpeed, float64(left-150)/float64(leftSpeed), float64(right-150)/float64(rightSpeed))
+		fmt.Printf("level: %d, left pwm: %d, right pwm: %d, left speed: %.4f, right speed: %.4f, left rate: %2f, right rate: %2f\n", i*100, left, right, leftSpeed, rightSpeed, float64(left-150)/float64(leftSpeed), float64(right-150)/float64(rightSpeed))
 
 	}
 	ld.Close()
 	rd.Close()
-	lb.Close()
-	rb.Close()
-
+	b.Close()
 }
